bb45857879a42bc98b80221378caf64e1214e8b5
Ziyang Liu <liuziyang9603090211@gmail.com>
2023-10-23 23:46:45 -0700

    changed layout

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ var config = {
     chapters: [
         {
             id: 'phl',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Philadelphia Bicycle Infrastructure',
             image: '',
             description: 'Getting around Philadelphia on two wheels is fast, fun, and cheap. As a typical East Coast large city, the urban core is dense, so there is a lot within reach of a 15 minute ride... even mountain bike trails. Paired with the public transit infrastructure, cycling can be more efficient and much less expensive than driving (and parking) a car.',
@@ -35,7 +35,7 @@ var config = {
         },
         {
             id: 'bike-lanes',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Bike Lanes',
             image: '',
             description: 'Philadelphia has XX miles of bike lanes, XX miles of which are protected. Drivers are getting more used to sharing the road, but ride defensively.',
@@ -55,7 +55,7 @@ var config = {
         },
         {
             id: 'indego',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Indego Bike Share',
             image: '',
             description: 'Indego has been operating in Philadelphia since 20XX. The system initally was focused on Center City, but has expanded service to neighboring areas to support equitable mobility options to the city\'s residents.',
@@ -80,7 +80,7 @@ var config = {
         },
         {
             id: 'belmont',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Belmont Plateau Trails',
             image: '',
             description: 'A short ride along the Schuylkill River Trail from the Art Museum, Belmont is a twisty, log-ridden rollercoaster of a trail network. It is easy to get turned around, the underbrush is at times impenetrable, and short steep sections come out of nowhere. In other words, it\'s really fun',
@@ -105,7 +105,7 @@ var config = {
         },
         {
             id: 'wiss',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Wissahickon Park Trails',
             image: '',
             description: 'This steep, rocky gorge can be surprisingly technical. Follow the orange and yellow trails to repeatedly climb and descend through the schist hillsides (careful of the cliffs), or stick to the gravel Forbidden Drive for a relaxing ride along the creek. You\'ll forget you\'re in a city.',
@@ -130,7 +130,7 @@ var config = {
         },
         {
             id: 'pennypack',
-            alignment: 'right',
+            alignment: 'left',
             title: 'Pennypack Park Trails',
             image: '',
             description: 'Pennypack is a great introduction trail system. Not too steep and not too technical, the beautiful wooded park also provides a great escape from urban life. The south side trails are originally bridle trails, so be nice to equestrians and dismount when you approach them.',
